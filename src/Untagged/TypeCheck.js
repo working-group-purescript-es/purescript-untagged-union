48e8b0ce15743ac76f31493a177a484d29391102
Mark Eibes <mark.eibes@gmail.com>
2021-12-28 20:15:22 +0100

    Convert FFI to es modules

--- src/Untagged/TypeCheck.js
+++ src/Untagged/TypeCheck.js
@@ -1,9 +1,9 @@
-exports.isInt = function (x) {
+export function isInt (x) {
   return typeof(x) == "number" && ((x|0) === x);
-};
+}
 
-exports.getProperty = function (name) {
+export function getProperty (name) {
   return function (x) {
     return x[name];
   };
-};
+}
